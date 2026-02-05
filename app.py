77a61c2be62ca1f04733c0f2404aacfd4ed67851
Tof55260 <c.boudot@live.fr>
2026-02-05 09:26:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -323,4 +323,6 @@ def admin_agents():
 # ================= RUN =================
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
